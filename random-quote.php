ca43d5c9c526e277d433cebaea4408bbcc33abd8
Adam Burucs <adam.burucs@gmail.com>
2015-08-14 16:39:42 +0200

    Random quote basic file
    Main program code

---
+++ random-quote.php
@@ -0,0 +1,29 @@
+/*
+Plugin Name: Adam's random quote
+Version: 0.1
+Plugin URI: http://burucs.com
+Description: Replaces the site's description with a random quote
+Author: Adam Burucs
+Author URI: http://burucs.com
+*/
+ 
+function ab_arq_generate() {
+    $quotes = array(
+        'Silence is golden.',
+        'Nothing is what it looks like.',
+        'Make everything right!',
+        'Better late than never.',
+        'Seeing is believing.'
+    );
+     
+    return $quotes[rand(0, count($quotes)-1)];
+}
+ 
+function ab_arq_change_bloginfo( $text, $show ) {
+    if( 'description' == $show ) {
+        $text = ab_arq_generate();
+    }
+    return $text;
+}
+ 
+add_filter( 'bloginfo', 'ab_arq_change_bloginfo', 10, 2 );
